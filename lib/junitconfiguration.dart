da494765d6a55742f4d9f591abce105683e3f386
Lukas Renggli <renggli@gmail.com>
2013-06-26 21:17:43 +0200

    - include skipped tests and improve test name

--- lib/junitconfiguration.dart
+++ lib/junitconfiguration.dart
@@ -14,23 +14,28 @@ class JUnitConfiguration extends Configuration {
   /**
    * Install this configuration with the testing framework.
    */
-  static void install([StringSink output, DateTime time]) {
-    unittestConfiguration = new JUnitConfiguration(output, time);
+  static void install({StringSink output, DateTime time, String hostname}) {
+    unittestConfiguration = new JUnitConfiguration(
+        output: output,
+        time: time,
+        hostname: hostname);
   }
 
   /**
    * Creates a new configuration instance with an optional output sink.
    */
-  factory JUnitConfiguration([StringSink output, DateTime time]) {
+  factory JUnitConfiguration({StringSink output, DateTime time, String hostname}) {
     return new JUnitConfiguration._internal(
         output != null ? output : stdout,
-        time != null ? time : new DateTime.now());
+        time != null ? time : new DateTime.now(),
+        hostname != null ? hostname : Platform.localHostname);
   }
 
   final StringSink _output;
   final DateTime _time;
+  final String _hostname;
 
-  JUnitConfiguration._internal(this._output, this._time);
+  JUnitConfiguration._internal(this._output, this._time, this._hostname);
 
   String get name => 'JUnit Test Configuration';
 
@@ -41,21 +46,30 @@ class JUnitConfiguration extends Configuration {
 
   @override
   void onSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
-    var totalTime = 0;
+    var totalTime = 0, skipped = 0;
     for (var testcase in results) {
       if (testcase.runningTime != null) {
         totalTime += testcase.runningTime.inMilliseconds;
       }
+      if (!testcase.enabled) {
+        skipped++;
+      }
     }
     _output.writeln('<?xml version="1.0" encoding="UTF-8" ?>');
-    _output.writeln('<testsuite name="All tests" tests="${results.length}" failures="$failed" errors="$errors" time="${totalTime / 1000.0}" timestamp="${_time}">');
-    for (var testcase in results) {
-      var runningTime = testcase.runningTime != null ? testcase.runningTime.inMilliseconds : 0;
-      _output.writeln('  <testcase id="${testcase.id}" name="${_xml(testcase.description)}" time="${runningTime / 1000.0}">');
-      if (testcase.result == 'fail') {
+    _output.writeln('<testsuite name="All tests" hostname="${_xml(this._hostname)}" tests="${results.length}" failures="$failed" errors="$errors" skipped="$skipped" time="${totalTime / 1000.0}" timestamp="${_time}">');
+    for (TestCase testcase in results) {
+      var time = testcase.runningTime != null ? testcase.runningTime.inMilliseconds : 0;
+      var name = testcase.description;
+      if (testcase.currentGroup != null && testcase.currentGroup != '') {
+        name = ' ${testcase.currentGroup}';
+      }
+      _output.writeln('  <testcase id="${testcase.id}" name="${_xml(name)}" time="${time / 1000.0}">');
+      if (testcase.result == FAIL) {
         _output.writeln('    <failure>${_xml(testcase.message)}</failure>');
-      } else if (testcase.result == 'error') {
+      } else if (testcase.result == ERROR) {
         _output.writeln('    <error>${_xml(testcase.message)}</error>');
+      } else if (!testcase.enabled) {
+        _output.writeln('    <skipped>${_xml(testcase.message)}</skipped>');
       }
       if (testcase.stackTrace != null && testcase.stackTrace != '') {
         _output.writeln('    <system-err>${_xml(testcase.stackTrace)}</system-err>');
@@ -73,8 +87,11 @@ class JUnitConfiguration extends Configuration {
     // nothing to be done
   }
 
-  String _xml(String string) {
-    return string
+  String _xml(value, [isNull = '']) {
+    if (value == null) {
+      value = isNull;
+   }
+    return value.toString()
         .replaceAll('&', '&amp;')
         .replaceAll('<', '&lt;')
         .replaceAll('>', '&gt;')
--- test/all_test.dart
+++ test/all_test.dart
@@ -11,28 +11,30 @@ void main() {
   test('empty test', () {
     var output = new StringBuffer();
     var time = new DateTime.now();
-    var config = new JUnitConfiguration(output, time);
+    var hostname = 'asterix';
+    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 2, 3, [], null);
     config.onDone(true);
     expect(output.toString(),
         '<?xml version="1.0" encoding="UTF-8" ?>\n' +
-        '<testsuite name="All tests" tests="0" failures="2" errors="3" time="0.0" timestamp="$time">\n' +
+        '<testsuite name="All tests" hostname="asterix" tests="0" failures="2" errors="3" skipped="0" time="0.0" timestamp="$time">\n' +
         '</testsuite>\n');
   });
 
   test('single test', () {
     var output = new StringBuffer();
     var time = new DateTime.now();
-    var config = new JUnitConfiguration(output, time);
+    var hostname = 'asterix';
+    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 0, 0, [currentTestCase], null);
     config.onDone(true);
     expect(output.toString(),
         '<?xml version="1.0" encoding="UTF-8" ?>\n' +
-        '<testsuite name="All tests" tests="1" failures="0" errors="0" time="0.0" timestamp="$time">\n' +
+        '<testsuite name="All tests" hostname="asterix" tests="1" failures="0" errors="0" skipped="0" time="0.0" timestamp="$time">\n' +
         '  <testcase id="2" name="single test" time="0.0">\n' +
         '  </testcase>\n' +
         '</testsuite>\n');
@@ -40,14 +42,15 @@ void main() {
   test('uncaught test', () {
     var output = new StringBuffer();
     var time = new DateTime.now();
-    var config = new JUnitConfiguration(output, time);
+    var hostname = 'asterix';
+    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 2, 3, [], 'uncaught failure');
     config.onDone(true);
     expect(output.toString(),
         '<?xml version="1.0" encoding="UTF-8" ?>\n' +
-        '<testsuite name="All tests" tests="0" failures="2" errors="3" time="0.0" timestamp="$time">\n' +
+        '<testsuite name="All tests" hostname="asterix" tests="0" failures="2" errors="3" skipped="0" time="0.0" timestamp="$time">\n' +
         '  <system-err>uncaught failure</system-err>\n' +
         '</testsuite>\n');
   });
