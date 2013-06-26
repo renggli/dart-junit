d2098b6b544b02f8a90040787874c287ec6f314d
Lukas Renggli <renggli@gmail.com>
2013-06-26 21:22:45 +0200

    - fixed bug and bumped to 0.5.4

--- lib/junitconfiguration.dart
+++ lib/junitconfiguration.dart
@@ -61,7 +61,7 @@ class JUnitConfiguration extends Configuration {
       var time = testcase.runningTime != null ? testcase.runningTime.inMilliseconds : 0;
       var name = testcase.description;
       if (testcase.currentGroup != null && testcase.currentGroup != '') {
-        name = ' ${testcase.currentGroup}';
+        name = '${testcase.currentGroup} ${name}';
       }
       _output.writeln('  <testcase id="${testcase.id}" name="${_xml(name)}" time="${time / 1000.0}">');
       if (testcase.result == FAIL) {
