d3c312a0780b7c5ba4437be80c904144371947ed
Lukas Renggli <renggli@gmail.com>
2015-02-27 19:16:19 +0100

    Made the default configuration exit

--- lib/junitconfiguration.dart
+++ lib/junitconfiguration.dart
@@ -1,146 +1,96 @@
 library junitconfiguration;
 
 import 'dart:io';
-import 'dart:async';
-import 'dart:isolate';
 import 'package:unittest/unittest.dart';
-import 'package:unittest/vm_config.dart';
+
+part 'exitconfiguration.dart';
+part 'nullconfiguration.dart';
 
 /**
- * A test configuration that emits JUnit compatible XML output.
+ * A test configuration that another test configuration and emits JUnit
+ * compatible XML output.
  */
-class JUnitConfiguration extends SimpleConfiguration {
-  @override
-  String get name => 'JUnit Test Configuration';
-
-  JUnitWriterDelegate _writer;
+class JUnitConfiguration implements Configuration {
 
   /**
-   * Install this configuration with the testing framework.
+   * Install this configuration.
    */
-  static void install({StringSink output, DateTime time, String hostname}) {
+  static void install({Configuration configuration, StringSink output, DateTime time,
+      String hostname}) {
     unittestConfiguration = new JUnitConfiguration(
+        configuration: configuration,
         output: output,
         time: time,
         hostname: hostname);
   }
 
+  /// The wrapped test configuration.
+  final Configuration configuration;
+
+  /// The string sink where the XML output is written to.
+  final StringSink output;
+
+  /// The current date and time of this operation.
+  final DateTime time;
+
+  /// The hostname where this operation is performed on.
+  final String hostname;
+
+  /// The output logged by test case.
+  final Map<TestCase, List<String>> logs = new Map();
+
   /**
    * Creates a new configuration instance with an optional output sink.
    */
-  factory JUnitConfiguration({StringSink output, DateTime time, String hostname}) {
+  factory JUnitConfiguration({Configuration configuration, StringSink output, DateTime time,
+        String hostname}) {
     return new JUnitConfiguration._internal(
+        configuration != null ? configuration : new ExitConfiguration(),
         output != null ? output : stdout,
         time != null ? time : new DateTime.now(),
         hostname != null ? hostname : Platform.localHostname);
   }
 
-  JUnitConfiguration._internal(output, time, hostname) : super() {
-    _writer = new JUnitWriterDelegate(output, time, hostname);
-  }
+  JUnitConfiguration._internal(this.configuration, this.output, this.time, this.hostname);
+
+  @override
+  bool get autoStart => configuration.autoStart;
+
+  @override
+  Duration get timeout => configuration.timeout;
+
+  @override
+  set timeout(Duration duration) => configuration.timeout = duration;
 
   @override
   void onInit() {
-    _writer.init();
+    logs.clear();
+    configuration.onInit();
   }
+
+  @override
+  void onStart() => configuration.onStart();
+
+  @override
+  void onTestStart(TestCase testCase) => configuration.onTestStart(testCase);
+
+  @override
+  void onTestResult(TestCase testCase) => configuration.onTestResult(testCase);
+
+  @override
+  void onTestResultChanged(TestCase testCase) => configuration.onTestResultChanged(testCase);
 
   @override
   void onLogMessage(TestCase testCase, String message) {
-    _writer.messageLogged(testCase, message);
+    logs.putIfAbsent(testCase, () => new List()).add(message);
+    configuration.onLogMessage(testCase, message);
   }
+
+  @override
+  void onDone(bool success) => configuration.onDone(success);
 
   @override
   void onSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
-    _writer.suiteSummary(passed, failed, errors, results, uncaughtError);
-  }
-
-  @override
-  void onDone(bool success) {
-    _writer.testDone(success);
-  }
-}
-
-/**
- * A test configuration for VM tests that emits JUnit compatible XML output.
- */
-class VMJUnitConfiguration extends VMConfiguration {
-  @override
-  String get name => 'JUnit VM Test Configuration';
-
-  JUnitWriterDelegate _writer;
-
-  /**
-   * Install this configuration with the testing framework.
-   */
-  static void install({StringSink output, DateTime time, String hostname}) {
-    unittestConfiguration = new VMJUnitConfiguration(
-        output: output,
-        time: time,
-        hostname: hostname);
-  }
-
-  /**
-   * Creates a new configuration instance with an optional output sink.
-   */
-  factory VMJUnitConfiguration({StringSink output, DateTime time, String hostname}) {
-    return new VMJUnitConfiguration._internal(
-        output != null ? output : stdout,
-        time != null ? time : new DateTime.now(),
-        hostname != null ? hostname : Platform.localHostname);
-  }
-
-  VMJUnitConfiguration._internal(output, time, hostname) : super() {
-    _writer = new JUnitWriterDelegate(output, time, hostname);
-  }
-
-  @override
-  void onInit() {
-    _writer.init();
-    super.onInit();
-  }
-
-  @override
-  void onLogMessage(TestCase testCase, String message) {
-    _writer.messageLogged(testCase, message);
-    super.onLogMessage(testCase, message);
-  }
-
-  @override
-  void onSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
-    _writer.suiteSummary(passed, failed, errors, results, uncaughtError);
-    super.onSummary(passed, failed, errors, results, uncaughtError);
-  }
-
-  @override
-  void onDone(bool success) {
-    _writer.testDone(success).then((_) {
-      super.onDone(success);
-    });
-  }
-}
-
-
-class JUnitWriterDelegate {
-  JUnitWriterDelegate(this._output, this._time, this._hostname);
-
-  StringSink _output;
-  DateTime _time;
-  String _hostname;
-
-  ReceivePort _receivePort;
-  Map<TestCase, List<String>> _stdout;
-
-  void init() {
-    filterStacks = false;
-    _receivePort = new ReceivePort();
-    _stdout = new Map();
-  }
-
-  void messageLogged(TestCase testCase, String message) {
-    _stdout.putIfAbsent(testCase, () => new List()).add(message);
-  }
-
-  void suiteSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
     var totalTime = 0, skipped = 0;
     for (var testCase in results) {
       if (testCase.runningTime != null) {
@@ -150,43 +100,34 @@ class JUnitWriterDelegate {
         skipped++;
       }
     }
-    _output.writeln('<?xml version="1.0" encoding="UTF-8" ?>');
-    _output.writeln('<testsuite name="All tests" hostname="${_xml(this._hostname)}" ' +
+    output.writeln('<?xml version="1.0" encoding="UTF-8" ?>');
+    output.writeln('<testsuite name="All tests" hostname="${_xml(hostname)}" ' +
         'tests="${results.length}" failures="$failed" errors="$errors" ' +
-        'skipped="$skipped" time="${totalTime / 1000.0}" timestamp="${_time}">');
+        'skipped="$skipped" time="${totalTime / 1000.0}" timestamp="${time}">');
     for (TestCase testCase in results) {
       var time = testCase.runningTime != null ? testCase.runningTime.inMilliseconds : 0;
-      _output.writeln('  <testcase id="${testCase.id}" name="${_xml(testCase.description)}" ' +
+      output.writeln('  <testcase id="${testCase.id}" name="${_xml(testCase.description)}" ' +
           'time="${time / 1000.0}">');
       if (testCase.result == FAIL) {
-        _output.writeln('    <failure>${_xml(testCase.message)}</failure>');
+        output.writeln('    <failure>${_xml(testCase.message)}</failure>');
       } else if (testCase.result == ERROR) {
-        _output.writeln('    <error>${_xml(testCase.message)}</error>');
+        output.writeln('    <error>${_xml(testCase.message)}</error>');
       } else if (!testCase.enabled) {
-        _output.writeln('    <skipped>${_xml(testCase.message)}</skipped>');
+        output.writeln('    <skipped>${_xml(testCase.message)}</skipped>');
       }
-      if (_stdout.containsKey(testCase)) {
-        var output = _stdout[testCase].join('\n');
-        _output.writeln('    <system-out>${_xml(output)}</system-out>');
+      if (logs.containsKey(testCase)) {
+        output.writeln('    <system-out>${_xml(logs[testCase].join('\n'))}</system-out>');
       }
       if (testCase.stackTrace != null) {
-        _output.writeln('    <system-err>${_xml(testCase.stackTrace)}</system-err>');
+        output.writeln('    <system-err>${_xml(testCase.stackTrace)}</system-err>');
       }
-      _output.writeln('  </testcase>');
+      output.writeln('  </testcase>');
     }
     if (uncaughtError != null && !uncaughtError.isEmpty) {
-      _output.writeln('  <system-err>${_xml(uncaughtError)}</system-err>');
+      output.writeln('  <system-err>${_xml(uncaughtError)}</system-err>');
     }
-    _output.writeln('</testsuite>');
-  }
-
-  Future testDone(bool success) {
-    _receivePort.close();
-    if (_output is IOSink) {
-      return (_output as IOSink).flush();
-    } else {
-      return new Future.value();
-    }
+    output.writeln('</testsuite>');
+    configuration.onSummary(passed, failed, errors, results, uncaughtError);
   }
 
   String _xml(value) {
@@ -196,5 +137,7 @@ class JUnitWriterDelegate {
         .replaceAll('>', '&gt;')
         .replaceAll('"', '&quot;');
   }
-
 }
+
+
+
--- test/all_test.dart
+++ test/all_test.dart
@@ -4,43 +4,48 @@ import 'package:unittest/unittest.dart';
 import 'package:junitconfiguration/junitconfiguration.dart';
 
 void main() {
+  var delegate = new NullConfiguration();
+  var time = new DateTime.now();
+  var hostname = 'asterix';
   test('empty test', () {
     var output = new StringBuffer();
-    var time = new DateTime.now();
-    var hostname = 'asterix';
-    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
+    var config = new JUnitConfiguration(configuration: delegate, output: output,
+        time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 2, 3, [], null);
     config.onDone(true);
-    expect(output.toString(), '<?xml version="1.0" encoding="UTF-8" ?>\n' +
+    expect(output.toString(),
+        '<?xml version="1.0" encoding="UTF-8" ?>\n'
         '<testsuite name="All tests" hostname="asterix" tests="0" failures="2" errors="3" skipped="0" time="0.0" timestamp="$time">\n'
-        + '</testsuite>\n');
+        '</testsuite>\n');
   });
   test('single test', () {
     var output = new StringBuffer();
-    var time = new DateTime.now();
-    var hostname = 'asterix';
-    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
+    var config = new JUnitConfiguration(configuration: delegate, output: output,
+        time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 0, 0, [currentTestCase], null);
     config.onDone(true);
-    expect(output.toString(), '<?xml version="1.0" encoding="UTF-8" ?>\n' +
+    expect(output.toString(),
+        '<?xml version="1.0" encoding="UTF-8" ?>\n'
         '<testsuite name="All tests" hostname="asterix" tests="1" failures="0" errors="0" skipped="0" time="0.0" timestamp="$time">\n'
-        + '  <testcase id="2" name="single test" time="0.0">\n' + '  </testcase>\n' + '</testsuite>\n');
+        '  <testcase id="2" name="single test" time="0.0">\n'
+        '  </testcase>\n'
+        '</testsuite>\n');
   });
   test('uncaught test', () {
     var output = new StringBuffer();
-    var time = new DateTime.now();
-    var hostname = 'asterix';
-    var config = new JUnitConfiguration(output: output, time: time, hostname: hostname);
+    var config = new JUnitConfiguration(configuration: delegate, output: output,
+        time: time, hostname: hostname);
     config.onInit();
     config.onStart();
     config.onSummary(1, 2, 3, [], 'uncaught failure');
     config.onDone(true);
-    expect(output.toString(), '<?xml version="1.0" encoding="UTF-8" ?>\n' +
+    expect(output.toString(), '<?xml version="1.0" encoding="UTF-8" ?>\n'
         '<testsuite name="All tests" hostname="asterix" tests="0" failures="2" errors="3" skipped="0" time="0.0" timestamp="$time">\n'
-        + '  <system-err>uncaught failure</system-err>\n' + '</testsuite>\n');
+        '  <system-err>uncaught failure</system-err>\n'
+        '</testsuite>\n');
   });
 }
