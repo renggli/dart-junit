8a3a8864e207016f427cb1aeb9890e7d9ce26ae2
Lukas Renggli <renggli@gmail.com>
2015-02-28 00:13:51 +0100

    Flush the output sink if necessary

--- lib/junitconfiguration.dart
+++ lib/junitconfiguration.dart
@@ -1,6 +1,7 @@
 library junitconfiguration;
 
 import 'dart:io';
+
 import 'package:unittest/unittest.dart';
 
 part 'exitconfiguration.dart';
@@ -25,19 +26,19 @@ class JUnitConfiguration implements Configuration {
   }
 
   /// The wrapped test configuration.
-  final Configuration configuration;
+  final Configuration _configuration;
 
   /// The string sink where the XML output is written to.
-  final StringSink output;
+  final StringSink _output;
 
   /// The current date and time of this operation.
-  final DateTime time;
+  final DateTime _time;
 
   /// The hostname where this operation is performed on.
-  final String hostname;
+  final String _hostname;
 
   /// The output logged by test case.
-  final Map<TestCase, List<String>> logs = new Map();
+  final Map<TestCase, List<String>> _logs = new Map();
 
   /**
    * Creates a new configuration instance with an optional output sink.
@@ -51,43 +52,51 @@ class JUnitConfiguration implements Configuration {
         hostname != null ? hostname : Platform.localHostname);
   }
 
-  JUnitConfiguration._internal(this.configuration, this.output, this.time, this.hostname);
+  JUnitConfiguration._internal(this._configuration, this._output, this._time, this._hostname);
 
   @override
-  bool get autoStart => configuration.autoStart;
+  bool get autoStart => _configuration.autoStart;
 
   @override
-  Duration get timeout => configuration.timeout;
+  Duration get timeout => _configuration.timeout;
 
   @override
-  set timeout(Duration duration) => configuration.timeout = duration;
+  set timeout(Duration duration) => _configuration.timeout = duration;
 
   @override
   void onInit() {
-    logs.clear();
-    configuration.onInit();
+    _logs.clear();
+    _configuration.onInit();
   }
 
   @override
-  void onStart() => configuration.onStart();
+  void onStart() => _configuration.onStart();
 
   @override
-  void onTestStart(TestCase testCase) => configuration.onTestStart(testCase);
+  void onTestStart(TestCase testCase) => _configuration.onTestStart(testCase);
 
   @override
-  void onTestResult(TestCase testCase) => configuration.onTestResult(testCase);
+  void onTestResult(TestCase testCase) => _configuration.onTestResult(testCase);
 
   @override
-  void onTestResultChanged(TestCase testCase) => configuration.onTestResultChanged(testCase);
+  void onTestResultChanged(TestCase testCase) => _configuration.onTestResultChanged(testCase);
 
   @override
   void onLogMessage(TestCase testCase, String message) {
-    logs.putIfAbsent(testCase, () => new List()).add(message);
-    configuration.onLogMessage(testCase, message);
+    _logs.putIfAbsent(testCase, () => new List()).add(message);
+    _configuration.onLogMessage(testCase, message);
   }
 
   @override
-  void onDone(bool success) => configuration.onDone(success);
+  void onDone(bool success) {
+    if (_output is IOSink) {
+      (_output as IOSink).flush().then((_) {
+        _configuration.onDone(success);
+      });
+    } else {
+      _configuration.onDone(success);
+    }
+  }
 
   @override
   void onSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
@@ -100,34 +109,34 @@ class JUnitConfiguration implements Configuration {
         skipped++;
       }
     }
-    output.writeln('<?xml version="1.0" encoding="UTF-8" ?>');
-    output.writeln('<testsuite name="All tests" hostname="${_xml(hostname)}" ' +
+    _output.writeln('<?xml version="1.0" encoding="UTF-8" ?>');
+    _output.writeln('<testsuite name="All tests" hostname="${_xml(_hostname)}" ' +
         'tests="${results.length}" failures="$failed" errors="$errors" ' +
-        'skipped="$skipped" time="${totalTime / 1000.0}" timestamp="${time}">');
+        'skipped="$skipped" time="${totalTime / 1000.0}" timestamp="${_time}">');
     for (TestCase testCase in results) {
       var time = testCase.runningTime != null ? testCase.runningTime.inMilliseconds : 0;
-      output.writeln('  <testcase id="${testCase.id}" name="${_xml(testCase.description)}" ' +
+      _output.writeln('  <testcase id="${testCase.id}" name="${_xml(testCase.description)}" ' +
           'time="${time / 1000.0}">');
       if (testCase.result == FAIL) {
-        output.writeln('    <failure>${_xml(testCase.message)}</failure>');
+        _output.writeln('    <failure>${_xml(testCase.message)}</failure>');
       } else if (testCase.result == ERROR) {
-        output.writeln('    <error>${_xml(testCase.message)}</error>');
+        _output.writeln('    <error>${_xml(testCase.message)}</error>');
       } else if (!testCase.enabled) {
-        output.writeln('    <skipped>${_xml(testCase.message)}</skipped>');
+        _output.writeln('    <skipped>${_xml(testCase.message)}</skipped>');
       }
-      if (logs.containsKey(testCase)) {
-        output.writeln('    <system-out>${_xml(logs[testCase].join('\n'))}</system-out>');
+      if (_logs.containsKey(testCase)) {
+        _output.writeln('    <system-out>${_xml(_logs[testCase].join('\n'))}</system-out>');
       }
       if (testCase.stackTrace != null) {
-        output.writeln('    <system-err>${_xml(testCase.stackTrace)}</system-err>');
+        _output.writeln('    <system-err>${_xml(testCase.stackTrace)}</system-err>');
       }
-      output.writeln('  </testcase>');
+      _output.writeln('  </testcase>');
     }
     if (uncaughtError != null && !uncaughtError.isEmpty) {
-      output.writeln('  <system-err>${_xml(uncaughtError)}</system-err>');
+      _output.writeln('  <system-err>${_xml(uncaughtError)}</system-err>');
     }
-    output.writeln('</testsuite>');
-    configuration.onSummary(passed, failed, errors, results, uncaughtError);
+    _output.writeln('</testsuite>');
+    _configuration.onSummary(passed, failed, errors, results, uncaughtError);
   }
 
   String _xml(value) {
